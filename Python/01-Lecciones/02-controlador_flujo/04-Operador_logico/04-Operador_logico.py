9d851ffb52c278027c29303ec8e7e18f0dc638a3
Aaron Bustos <ar.bustos@duocuc.cl>
2023-12-18 23:29:37 -0300

    se carga nueva clase, operador logico

--- Python/01-Lecciones/02-controlador_flujo/04-Operador_logico/04-Operador_logico.py
+++ Python/01-Lecciones/02-controlador_flujo/04-Operador_logico/04-Operador_logico.py
@@ -1,1 +1,25 @@
-# and, or , not 
+# Ejemplo de 'and'
+edad = 25
+tiene_licencia = True
+
+if edad >= 18 and tiene_licencia:
+    print("Puede conducir.")
+else:
+    print("No puede conducir.")
+
+# Ejemplo de 'or'
+tiene_pasaporte = False
+tiene_cedula = True
+
+if tiene_pasaporte or tiene_cedula:
+    print("Puede viajar.")
+else:
+    print("No puede viajar.")
+
+# Ejemplo de 'not'
+es_dia_laboral = False
+
+if not es_dia_laboral:
+    print("Es fin de semana.")
+else:
+    print("Es un día laboral.")
