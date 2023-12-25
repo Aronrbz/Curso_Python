ca5c801832dcbef69e3708edbba536dcc77b041b
Aaron Bustos <ar.bustos@duocuc.cl>
2023-12-24 21:27:44 -0300

    se sube contenido de loops anidados

--- Python/01-Lecciones/02-controlador_flujo/07-loop_while/07-loop_while.py
+++ Python/01-Lecciones/02-controlador_flujo/07-loop_while/07-loop_while.py
@@ -8,12 +8,36 @@
 #     comando = input("$ ")
 #     print(comando)
 
+################################################################
 # Ejercicios para ti:
 
 # Fácil: Escribe un bucle while que comience con x = 0 e imprima x mientras x sea menor a 5.
 
+# x = 0
+# while x < 5:
+#     print(x)
+#     x += 1
+
+
 # Medio: Utiliza un bucle while para crear un pequeño programa que pida al usuario que ingrese números hasta que 
 #         escriba "stop". Imprime cada número ingresado.
 
+
+# numeros = ""
+# while numeros.lower() != "stop":
+#     numeros = input("Ingresa cualquier número. Escribe 'stop' para salir: ")
+#     print(numeros)
+
+
 # Difícil: Haz un bucle while que sume números del 1 al 10 (inclusive). Utiliza una variable para llevar la cuenta 
-#         de la suma y otra para llevar la cuenta del número actual. Imprime la suma total después de salir del bucle.
+#         # de la suma y otra para llevar la cuenta del número actual. Imprime la suma total después de salir del bucle.
+
+# numero = 0
+# suma = 0
+# while numero <= 10:
+#     suma += numero   # Suma el número actual a la suma total
+#     print("Número actual:", numero, "Suma parcial:", suma)
+#     numero += 1  # Incrementa el número en 1
+# print("Suma final:", suma)
+
+##############################################################
