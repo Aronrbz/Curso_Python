a0d2ec642e4f3b38a3d59a80f750b8e425594bb2
Aron Bustos <ar.bustos@duocuc.cl>
2024-01-03 18:07:09 -0300

    se suben respuestas de primera evaluacion

--- Python/03-Evaluaciones/Primera_evaluacion/PrimeraEvaluacion.py
+++ Python/03-Evaluaciones/Primera_evaluacion/PrimeraEvaluacion.py
@@ -28,16 +28,61 @@
 # Escribe un programa que pida al usuario dos números y muestre la suma, resta, multiplicación, y división de esos 
 # números.
 
+# R: 
+# num1 = int(input("Ingrese el primer número: "))
+# num2 = int(input("ingrese el segundo número: "))
+
+# suma = num1 + num2
+# resta = num1 - num2
+# mult = num1 * num2
+# div = num1 / num2
+
+# print(f"La suma de {num1} y {num2} es: {suma}")
+# print(f"La resta de {num1} y {num2} es: {resta}")
+# print(f"La multiplicación de {num1} y {num2} es: {mult}")
+# print(f"La división de {num1} y {num2} es: {div}")
+
+
 # Trabajando con Strings:
 # Dada la frase "Hola, mundo", escribe un fragmento de código que imprima la longitud de esta frase y luego la misma 
 # frase en mayúsculas.
 
+# Frase = "Hola, mundo"
+
+# print(len(Frase))
+# print(Frase.upper())
+
+
 # Uso de for y range:
 # Utiliza un bucle for y la función range para imprimir los números pares del 1 al 10.
+
+# for i in range(11):
+#     if i % 2 == 0:
+#         print(i)
+
 
 # Condiciones Anidadas:
 # Escribe un programa que solicite la edad del usuario. Si el usuario tiene más de 18 años, imprime "Adulto". Si tiene 
 # entre 13 y 18 años, imprime "Adolescente". Si tiene menos de 13, imprime "Niño".
 
+# persona = int(input("Ingrese su edad: "))
+
+# if persona > 18:
+#     print("Adulto")
+# elif persona < 18 or persona >= 13:
+#     print("Adolescente")
+# else:
+#     print("Niño")
+
 # Loops Anidados:
 # Escribe un programa que utilice bucles anidados para imprimir un patrón de asteriscos que forme un cuadrado de 3x3.
+
+# Bucle externo para las filas
+# for fila in range(3):
+#     # Inicializar una string vacía para la fila actual
+#     fila_str = ""
+#     # Bucle interno para las columnas
+#     for columna in range(3):
+#         fila_str += "* "  # Concatenar un asterisco y un espacio
+#     # Imprimir la fila completa
+#     print(fila_str)
---
+++ Python/testing.py
@@ -0,0 +1,6 @@
+# Uso de for y range:
+# Utiliza un bucle for y la función range para imprimir los números pares del 1 al 10.
+
+for i in range(11):
+    if i % 2 == 0:
+        print(i)
