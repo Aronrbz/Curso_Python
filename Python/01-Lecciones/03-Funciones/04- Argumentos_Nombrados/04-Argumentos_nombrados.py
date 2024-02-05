95261a21a1416c09a2c750eb8f6315156b7374e3
Aron Bustos <ar.bustos@duocuc.cl>
2024-02-05 17:58:17 -0300

    se agrega xargs a las lecciones

--- Python/01-Lecciones/03-Funciones/04- Argumentos_Nombrados/04-Argumentos_nombrados.py
+++ Python/01-Lecciones/03-Funciones/04- Argumentos_Nombrados/04-Argumentos_nombrados.py
@@ -9,8 +9,18 @@ mostrar_informacion(empresa="Talent", nombre="Aron Bustos")
 
 
 # Medio   : Escribe una función configurar_alarma que acepte tres parámetros: hora, minutos y sonido, donde sonido tiene 
-#     un valor predeterminado. Llama a la función especificando minutos y sonido, omitiendo hora.
+#     un valor predeterminado. Llama a la función especificando minutos y sonido, omitiendo hora.}
+
+def configurar_alarma(hora=0, minuto=0, sonido="nokia ladrillo"):
+    print(f"hora: {hora} Minuto: {minuto} Sonido: {sonido}")
+
+configurar_alarma(sonido="El baile del perro", minuto=40)
 
 # Difícil : Crea una función preparar_bebida que tenga tres parámetros: tipo, azucar y leche, donde azucar y leche son 
 #     argumentos opcionales con valores predeterminados. Llama a esta función pasando tipo como argumento posicional y 
 #     modificando el valor predeterminado de leche mediante un argumento nombrado.
+
+def preparar_bebidas(tipo, azucar="un kilo", leche="Colun"):
+    print(f"tipo: {tipo}, azucar: {azucar}, leche: {leche}")
+
+preparar_bebidas("coca cola", leche="Surlat", azucar="medio kilo")
