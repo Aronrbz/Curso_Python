0797e928aece689e0f16ad78a8e8ca9e7796e309
Aron Bustos <ar.bustos@duocuc.cl>
2024-04-08 18:03:46 -0400

    se agrega kargs

--- Python/01-Lecciones/03-Funciones/06-kargs/06-kwargs.py
+++ Python/01-Lecciones/03-Funciones/06-kargs/06-kwargs.py
@@ -1,4 +1,25 @@
-def crear_perfil(**kwargs):
-        print(kwargs)
+# Fácil: Define una función imprimir_detalles que use **kwargs para aceptar detalles de un libro como título, 
+#     autor y año, e imprima estos detalles en un formato amigable.
 
-crear_perfil(nombre="Laura", edad=25, profesion="Ingeniera")
+# def imprimir_detalles(**kwargs):
+#         print(kwargs)
+
+# imprimir_detalles(Titulo="Papelucho", Autor="Marcela Paz", Año="1947")
+
+
+
+# Medio: Crea una función calcular_precio que utilice **kwargs para aceptar el precio base de un producto, y opcionalmente, 
+#     un descuento en porcentaje. La función debe calcular y mostrar el precio final después del descuento.
+
+
+# def calcular_precio(**kwargs):
+#         print()
+
+# calcular_precio = 
+
+
+
+
+# Difícil: Escribe una función crear_perfil_usuario que acepte un nombre obligatorio y luego cualquier número de detalles 
+#     adicionales sobre el usuario (como email, ciudad, edad) usando **kwargs. La función debe imprimir el perfil completo 
+#     del usuario.
