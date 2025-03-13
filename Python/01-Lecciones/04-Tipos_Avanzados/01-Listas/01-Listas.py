17a0aae6a5797e62078afa346c91bca41d94c2df
Aaron Bustos <ar.bustos@duocuc.cl>
2025-03-13 19:04:31 -0300

    se suben nuevos temas, listas

---
+++ Python/01-Lecciones/04-Tipos_Avanzados/01-Listas/01-Listas.py
@@ -0,0 +1,7 @@
+# Ejercicios de Práctica con Listas
+
+#  Fácil: Crea una lista llamada colores con tres colores. Imprime el segundo color de la lista.
+
+#  Medio: Crea una lista de números del 1 al 10 usando range(). Luego, suma otra lista con los primeros tres nombres de personas que elijas y muestra el resultado.
+
+#  Difícil: Usa la multiplicación de listas para crear una lista de 15 elementos alternando "X" y "O", e imprime el resultado.
